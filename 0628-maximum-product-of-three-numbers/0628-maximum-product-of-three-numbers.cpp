eb69fa0d02d783e5ef4e6bb1663048c18e9e1c33
ruchakothikar <98452719+ruchakothikar@users.noreply.github.com>
2025-09-14 11:27:46 -0700

    Time: 8 ms (48.49%), Space: 31.5 MB (39.28%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -10,22 +10,3 @@ public:
 
 //sort vector in increasing order
 //return the max of either the last 3 numbers, or the first 2 and one last number
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
- sort(nums.begin(), nums.end());
-        return max((nums[nums.size()-1]*nums[nums.size()-2]*nums[nums.size()-3]), (nums[nums.size()-1]*nums[0]*nums[1]));
-*/
