0c924d9e23e51e47ae9b4c507c3b45b9c1acf0a8
ruchakothikar <98452719+ruchakothikar@users.noreply.github.com>
2026-04-03 19:21:05 -0700

    Time: 3 ms (91.09%), Space: 33.1 MB (24.91%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,31 @@
+class KthLargest {
+    int k;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(int i=0; i<nums.size(); i++)
+        {
+            minHeap.push(nums[i]);
+            if(minHeap.size()>k)
+            {
+                minHeap.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+        if(minHeap.size()>k)
+        {
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
